7bf9e3d6c380f5a6f08843770af17809c4658a6c
AmedeeRoy <amedee.roy@protonmail.com>
2025-03-25 13:04:28 +0000

    new script for leave-one-out multirun

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 # TRAIN
-# Main training with all data
+# Test training with default
 python src/train.py
 
-python src/train.py data="defile-small"
-
-python src/train.py debug=default
-
-# PREDICT
-python src/predict.py data.species="Common Buzzard"
-
-# PRODUCTION
-python src/train.py --multirun experiment=production data.species="Common Buzzard","Red Kite","Black Kite","European Honey-buzzard","Western Marsh Harrier","Eurasian Sparrowhawk","Eurasian Kestrel","Osprey","Eurasian Hobby","Hen Harrier","Merlin" logger=csv
-python src/predict.py --multirun experiment=production data.species="Common Buzzard","Red Kite","Black Kite","European Honey-buzzard","Western Marsh Harrier","Eurasian Sparrowhawk","Eurasian Kestrel","Osprey","Eurasian Hobby","Hen Harrier","Merlin" logger=csv
-
-
 # HYPERPARAMETERS OPTIMIZATION
-# logs\train\multiruns\2024-09-11_14-58-53
+# To see the results:
+# cd /logs/mlflow
+# mlflow ui --port=3000
 python src/train.py data.species="Common Buzzard" hparams_search=unet logger=mlflow
 python src/train.py data.species="Red Kite" hparams_search=unet logger=mlflow
 python src/train.py data.species="Black Kite" hparams_search=unet logger=mlflow
@@ -28,7 +18,9 @@ python src/train.py data.species="Osprey" hparams_search=unet logger=mlflow
 python src/train.py data.species="Hen Harrier" hparams_search=unet logger=mlflow
 python src/train.py data.species="Merlin" hparams_search=unet logger=mlflow
 
+# PREDICT
+python src/predict.py data.species="Common Buzzard"
 
-# tensorboard --logdir=logs/train/multiruns
-# python src/eval.py model=unet logger=csv
-
+# PRODUCTION
+python src/train.py --multirun experiment=production data.species="Common Buzzard","Red Kite","Black Kite","European Honey-buzzard","Western Marsh Harrier","Eurasian Sparrowhawk","Eurasian Kestrel","Osprey","Eurasian Hobby","Hen Harrier","Merlin" logger=csv
+python src/predict.py --multirun experiment=production data.species="Common Buzzard","Red Kite","Black Kite","European Honey-buzzard","Western Marsh Harrier","Eurasian Sparrowhawk","Eurasian Kestrel","Osprey","Eurasian Hobby","Hen Harrier","Merlin" logger=csv
