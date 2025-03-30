83a7e5f037d7a9ffe531c1085bfcdb5e0bf0f86d
AmedeeRoy <amedee.roy@protonmail.com>
2025-03-30 19:21:02 +0000

    modify www

--- www/script.js
+++ www/script.js
@@ -5,5 +5,25 @@ window.onload = function() {
         var displayTime = date.toLocaleTimeString();
 
         document.getElementById('datetime').innerHTML = displayDate + " " + displayTime;
-    }, 1000); // 1000 milliseconds = 1 second
+    }, 1000); // 1000 milliseconds = 1 second   
 }
+
+function findFileWithCurrentDate() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const datePrefix = `${year}_${month}_${day}`;
+
+    return `img/${year}${month}${day}_Common_Buzzard.jpg`;
+}
+
+const fileFound = findFileWithCurrentDate();
+document.write(`<img src="${fileFound}" alt="Common Buzzard">`);
+
+// fetchFile(function(){
+//     // const fileFound = findFileWithCurrentDate(files);
+//     document.getElementById('myfile').innerHTML = "fileFound";
+// }); // 1000 milliseconds = 1 second
+
+
